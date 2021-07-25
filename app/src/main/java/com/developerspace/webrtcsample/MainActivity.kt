b01da312645dbee73a072f0b1cfede1869e389a8
http600 <http418@gmail.com>
2021-07-25 17:51:01 +0800

    commit

--- app/src/main/java/com/developerspace/webrtcsample/MainActivity.kt
+++ app/src/main/java/com/developerspace/webrtcsample/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.developerspace.webrtcsample
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.parse.Parse
+import com.parse.ParseObject
 import kotlinx.android.synthetic.main.activity_start.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,25 +20,33 @@ class MainActivity : AppCompatActivity() {
         Constants.isIntiatedNow = true
         Constants.isCallEnded = true
         start_meeting.setOnClickListener {
+            /*val parseObject = ParseObject("calls2021")
+            parseObject.put("a", "b")
+            parseObject.save()
+            Log.e("parse x", parseObject.toString())*/
             if (meeting_id.text.toString().trim().isNullOrEmpty())
                 meeting_id.error = "Please enter meeting id"
             else {
+                /*val intent = Intent(this@MainActivity, RTCActivity::class.java)
+                intent.putExtra("meetingID",meeting_id.text.toString())
+                intent.putExtra("isJoin",false)
+                startActivity(intent)*/
                 db.collection("calls")
-                        .document(meeting_id.text.toString())
-                        .get()
-                        .addOnSuccessListener {
-                            if (it["type"]=="OFFER" || it["type"]=="ANSWER" || it["type"]=="END_CALL") {
-                                meeting_id.error = "Please enter new meeting ID"
-                            } else {
-                                val intent = Intent(this@MainActivity, RTCActivity::class.java)
-                                intent.putExtra("meetingID",meeting_id.text.toString())
-                                intent.putExtra("isJoin",false)
-                                startActivity(intent)
-                            }
-                        }
-                        .addOnFailureListener {
+                    .document(meeting_id.text.toString())
+                    .get()
+                    .addOnSuccessListener {
+                        if (it["type"] == "OFFER" || it["type"] == "ANSWER" || it["type"] == "END_CALL") {
                             meeting_id.error = "Please enter new meeting ID"
+                        } else {
+                            val intent = Intent(this@MainActivity, RTCActivity::class.java)
+                            intent.putExtra("meetingID", meeting_id.text.toString())
+                            intent.putExtra("isJoin", false)
+                            startActivity(intent)
                         }
+                    }
+                    .addOnFailureListener {
+                        meeting_id.error = "Please enter new meeting ID"
+                    }
             }
         }
         join_meeting.setOnClickListener {
@@ -43,10 +54,18 @@ class MainActivity : AppCompatActivity() {
                 meeting_id.error = "Please enter meeting id"
             else {
                 val intent = Intent(this@MainActivity, RTCActivity::class.java)
-                intent.putExtra("meetingID",meeting_id.text.toString())
-                intent.putExtra("isJoin",true)
+                intent.putExtra("meetingID", meeting_id.text.toString())
+                intent.putExtra("isJoin", true)
                 startActivity(intent)
             }
         }
+
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId("LyeURsBTb01F6o3PQahLMLAoxpiJr5X5f01bmI2u") // if desired
+//                .clientKey("YOUR_CLIENT_KEY")
+                .server("http://101.34.243.201:1337/parse")
+                .build()
+        )
     }
 }
--- app/src/main/java/com/developerspace/webrtcsample/RTCClient.kt
+++ app/src/main/java/com/developerspace/webrtcsample/RTCClient.kt
@@ -5,12 +5,14 @@ import android.content.Context
 import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.parse.ParseObject
+import com.parse.ParseQuery
 import org.webrtc.*
 
 
 class RTCClient(
-        context: Application,
-        observer: PeerConnection.Observer
+    context: Application,
+    observer: PeerConnection.Observer
 ) {
 
     companion object {
@@ -20,11 +22,11 @@ class RTCClient(
 
     private val rootEglBase: EglBase = EglBase.create()
 
-    private var localAudioTrack : AudioTrack? = null
-    private var localVideoTrack : VideoTrack? = null
+    private var localAudioTrack: AudioTrack? = null
+    private var localVideoTrack: VideoTrack? = null
     val TAG = "RTCClient"
 
-    var remoteSessionDescription : SessionDescription? = null
+    var remoteSessionDescription: SessionDescription? = null
 
     val db = Firebase.firestore
 
@@ -33,50 +35,57 @@ class RTCClient(
     }
 
     private val iceServer = listOf(
-            PeerConnection.IceServer.builder("stun:stun.l.google.com:19302")
-                    .createIceServer()
+        PeerConnection.IceServer.builder("stun:stun.l.google.com:19302")
+            .createIceServer()
     )
 
     private val peerConnectionFactory by lazy { buildPeerConnectionFactory() }
     private val videoCapturer by lazy { getVideoCapturer(context) }
 
-    private val audioSource by lazy { peerConnectionFactory.createAudioSource(MediaConstraints())}
+    private val audioSource by lazy { peerConnectionFactory.createAudioSource(MediaConstraints()) }
     private val localVideoSource by lazy { peerConnectionFactory.createVideoSource(false) }
     private val peerConnection by lazy { buildPeerConnection(observer) }
 
     private fun initPeerConnectionFactory(context: Application) {
         val options = PeerConnectionFactory.InitializationOptions.builder(context)
-                .setEnableInternalTracer(true)
-                .setFieldTrials("WebRTC-H264HighProfile/Enabled/")
-                .createInitializationOptions()
+            .setEnableInternalTracer(true)
+            .setFieldTrials("WebRTC-H264HighProfile/Enabled/")
+            .createInitializationOptions()
         PeerConnectionFactory.initialize(options)
     }
 
     private fun buildPeerConnectionFactory(): PeerConnectionFactory {
         return PeerConnectionFactory
-                .builder()
-                .setVideoDecoderFactory(DefaultVideoDecoderFactory(rootEglBase.eglBaseContext))
-                .setVideoEncoderFactory(DefaultVideoEncoderFactory(rootEglBase.eglBaseContext, true, true))
-                .setOptions(PeerConnectionFactory.Options().apply {
-                    disableEncryption = true
-                    disableNetworkMonitor = true
-                })
-                .createPeerConnectionFactory()
+            .builder()
+            .setVideoDecoderFactory(DefaultVideoDecoderFactory(rootEglBase.eglBaseContext))
+            .setVideoEncoderFactory(
+                DefaultVideoEncoderFactory(
+                    rootEglBase.eglBaseContext,
+                    true,
+                    true
+                )
+            )
+            .setOptions(PeerConnectionFactory.Options().apply {
+                disableEncryption = true
+                disableNetworkMonitor = true
+            })
+            .createPeerConnectionFactory()
     }
 
-    private fun buildPeerConnection(observer: PeerConnection.Observer) = peerConnectionFactory.createPeerConnection(
+    private fun buildPeerConnection(observer: PeerConnection.Observer) =
+        peerConnectionFactory.createPeerConnection(
             iceServer,
             observer
-    )
+        )
 
     private fun getVideoCapturer(context: Context) =
-            Camera2Enumerator(context).run {
-                deviceNames.find {
-                    isFrontFacing(it)
-                }?.let {
-                    createCapturer(it, null)
-                } ?: throw IllegalStateException()
-            }
+        Camera2Enumerator(context).run {
+            deviceNames.find {
+                isFrontFacing(it)
+            }?.let {
+                createCapturer(it, null)
+            } ?: throw IllegalStateException()
+        }
 
     fun initSurfaceView(view: SurfaceViewRenderer) = view.run {
         setMirror(true)
@@ -85,10 +94,16 @@ class RTCClient(
     }
 
     fun startLocalVideoCapture(localVideoOutput: SurfaceViewRenderer) {
-        val surfaceTextureHelper = SurfaceTextureHelper.create(Thread.currentThread().name, rootEglBase.eglBaseContext)
-        (videoCapturer as VideoCapturer).initialize(surfaceTextureHelper, localVideoOutput.context, localVideoSource.capturerObserver)
+        val surfaceTextureHelper =
+            SurfaceTextureHelper.create(Thread.currentThread().name, rootEglBase.eglBaseContext)
+        (videoCapturer as VideoCapturer).initialize(
+            surfaceTextureHelper,
+            localVideoOutput.context,
+            localVideoSource.capturerObserver
+        )
         videoCapturer.startCapture(320, 240, 60)
-        localAudioTrack = peerConnectionFactory.createAudioTrack(LOCAL_TRACK_ID + "_audio", audioSource);
+        localAudioTrack =
+            peerConnectionFactory.createAudioTrack(LOCAL_TRACK_ID + "_audio", audioSource);
         localVideoTrack = peerConnectionFactory.createVideoTrack(LOCAL_TRACK_ID, localVideoSource)
         localVideoTrack?.addSink(localVideoOutput)
         val localStream = peerConnectionFactory.createLocalMediaStream(LOCAL_STREAM_ID)
@@ -111,17 +126,24 @@ class RTCClient(
 
                     override fun onSetSuccess() {
                         val offer = hashMapOf(
-                                "sdp" to desc?.description,
-                                "type" to desc?.type
+                            "sdp" to desc?.description,
+                            "type" to desc?.type
                         )
+                        val parseObject = ParseObject("calls2021")
+                        parseObject.put("key", meetingID.toString())
+                        offer.forEach { (k, v) -> parseObject.put(k, v.toString()) }
+                        parseObject.saveInBackground().onSuccessTask {
+                            Log.e(TAG, "parse platform save success")
+                            return@onSuccessTask it
+                        }
                         db.collection("calls").document(meetingID)
-                                .set(offer)
-                                .addOnSuccessListener {
-                                    Log.e(TAG, "DocumentSnapshot added")
-                                }
-                                .addOnFailureListener { e ->
-                                    Log.e(TAG, "Error adding document", e)
-                                }
+                            .set(offer)
+                            .addOnSuccessListener {
+                                Log.e(TAG, "DocumentSnapshot added")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.e(TAG, "Error adding document", e)
+                            }
                         Log.e(TAG, "onSetSuccess")
                     }
 
@@ -153,17 +175,19 @@ class RTCClient(
         createAnswer(object : SdpObserver by sdpObserver {
             override fun onCreateSuccess(desc: SessionDescription?) {
                 val answer = hashMapOf(
-                        "sdp" to desc?.description,
-                        "type" to desc?.type
+                    "sdp" to desc?.description,
+                    "type" to desc?.type
                 )
+                val query = ParseQuery.getQuery<ParseObject>("calls2021")
+                query.whereEqualTo("key", meetingID)
                 db.collection("calls").document(meetingID)
-                        .set(answer)
-                        .addOnSuccessListener {
-                            Log.e(TAG, "DocumentSnapshot added")
-                        }
-                        .addOnFailureListener { e ->
-                            Log.e(TAG, "Error adding document", e)
-                        }
+                    .set(answer)
+                    .addOnSuccessListener {
+                        Log.e(TAG, "DocumentSnapshot added")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e(TAG, "Error adding document", e)
+                    }
                 setLocalDescription(object : SdpObserver {
                     override fun onSetFailure(p0: String?) {
                         Log.e(TAG, "onSetFailure: $p0")
@@ -190,9 +214,11 @@ class RTCClient(
         }, constraints)
     }
 
-    fun call(sdpObserver: SdpObserver, meetingID: String) = peerConnection?.call(sdpObserver, meetingID)
+    fun call(sdpObserver: SdpObserver, meetingID: String) =
+        peerConnection?.call(sdpObserver, meetingID)
 
-    fun answer(sdpObserver: SdpObserver, meetingID: String) = peerConnection?.answer(sdpObserver, meetingID)
+    fun answer(sdpObserver: SdpObserver, meetingID: String) =
+        peerConnection?.answer(sdpObserver, meetingID)
 
     fun onRemoteSessionReceived(sessionDescription: SessionDescription) {
         remoteSessionDescription = sessionDescription
@@ -222,36 +248,48 @@ class RTCClient(
 
     fun endCall(meetingID: String) {
         db.collection("calls").document(meetingID).collection("candidates")
-                .get().addOnSuccessListener {
-                    val iceCandidateArray: MutableList<IceCandidate> = mutableListOf()
-                    for ( dataSnapshot in it) {
-                        if (dataSnapshot.contains("type") && dataSnapshot["type"]=="offerCandidate") {
-                            val offerCandidate = dataSnapshot
-                            iceCandidateArray.add(IceCandidate(offerCandidate["sdpMid"].toString(), Math.toIntExact(offerCandidate["sdpMLineIndex"] as Long), offerCandidate["sdp"].toString()))
-                        } else if (dataSnapshot.contains("type") && dataSnapshot["type"]=="answerCandidate") {
-                            val answerCandidate = dataSnapshot
-                            iceCandidateArray.add(IceCandidate(answerCandidate["sdpMid"].toString(), Math.toIntExact(answerCandidate["sdpMLineIndex"] as Long), answerCandidate["sdp"].toString()))
-                        }
+            .get().addOnSuccessListener {
+                val iceCandidateArray: MutableList<IceCandidate> = mutableListOf()
+                for (dataSnapshot in it) {
+                    if (dataSnapshot.contains("type") && dataSnapshot["type"] == "offerCandidate") {
+                        val offerCandidate = dataSnapshot
+                        iceCandidateArray.add(
+                            IceCandidate(
+                                offerCandidate["sdpMid"].toString(),
+                                Math.toIntExact(offerCandidate["sdpMLineIndex"] as Long),
+                                offerCandidate["sdp"].toString()
+                            )
+                        )
+                    } else if (dataSnapshot.contains("type") && dataSnapshot["type"] == "answerCandidate") {
+                        val answerCandidate = dataSnapshot
+                        iceCandidateArray.add(
+                            IceCandidate(
+                                answerCandidate["sdpMid"].toString(),
+                                Math.toIntExact(answerCandidate["sdpMLineIndex"] as Long),
+                                answerCandidate["sdp"].toString()
+                            )
+                        )
                     }
-                    peerConnection?.removeIceCandidates(iceCandidateArray.toTypedArray())
                 }
+                peerConnection?.removeIceCandidates(iceCandidateArray.toTypedArray())
+            }
         val endCall = hashMapOf(
-                "type" to "END_CALL"
+            "type" to "END_CALL"
         )
         db.collection("calls").document(meetingID)
-                .set(endCall)
-                .addOnSuccessListener {
-                    Log.e(TAG, "DocumentSnapshot added")
-                }
-                .addOnFailureListener { e ->
-                    Log.e(TAG, "Error adding document", e)
-                }
+            .set(endCall)
+            .addOnSuccessListener {
+                Log.e(TAG, "DocumentSnapshot added")
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Error adding document", e)
+            }
 
         peerConnection?.close()
     }
 
     fun enableVideo(videoEnabled: Boolean) {
-        if (localVideoTrack !=null)
+        if (localVideoTrack != null)
             localVideoTrack?.setEnabled(videoEnabled)
     }
 
@@ -259,6 +297,7 @@ class RTCClient(
         if (localAudioTrack != null)
             localAudioTrack?.setEnabled(audioEnabled)
     }
+
     fun switchCamera() {
         videoCapturer.switchCamera(null)
     }
